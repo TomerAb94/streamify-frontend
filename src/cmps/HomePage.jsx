d00f565df5a376f5b2d0ee723416b0ec92729c10
Dorazar <dorazar01@gmail.com>
2025-09-30 19:54:33 +0300

    add stations to homepage

--- src/cmps/HomePage.jsx
+++ src/cmps/HomePage.jsx
@@ -1,7 +1,24 @@
+
+import { useOutletContext } from "react-router-dom"
+import { StationPreview } from "./StationPreview"
+
+
 export function HomePage() {
+
+const {stations} = useOutletContext()
+
+
+
+if (!stations) return <>loading...</>
     return (
         <section className="home">
-            <h1>Home sweet Home</h1>
+            
+            <h1>Home sweet Home </h1>
+    {stations.map(station =>
+                   <li key={station._id}>
+                       {station.title}
+                   </li>)
+               }
         </section >
     )
 }
