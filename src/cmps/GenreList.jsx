b6ae5361ca94bc91b956f6cb746c14eccd10632d
Dorazar <dorazar01@gmail.com>
2025-10-17 15:46:51 +0300

    Add  button to playlist

--- src/cmps/GenreList.jsx
+++ src/cmps/GenreList.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { spotifyService } from '../services/spotify.service'
 import { Link, NavLink, useLocation, useParams } from 'react-router-dom'
-
+import { SvgIcon } from './SvgIcon'
 
 
 export function GenreList( ) {
@@ -36,13 +36,20 @@ export function GenreList( ) {
       </div>
             <div className="playlists-container">
             {playlists.map((station) => (
+                
                 <NavLink className="playlist-item" to={`/browse/genre/${params.genreName}/${station.id}`} key={station.id}>
-                   
-                        {console.log('station:', station)}
-                        {station.images?.[0]?.url && (
-                            <img src={station.images[0].url} alt={station.name} />
-                        )}
+
+                       <div className='playlist-img-container'>
+                           {station.images?.[0]?.url && (
+                               <img src={station.images[0].url} alt={station.name} />
+                           )}
+                               <SvgIcon iconName="play" className="play-container"  />
+                       </div>
+
+                       
+                       
                         <h3 className='playlist-name'>{station.name}</h3>
+                        
                          <h3 className='playlist-description'>{station.description?station.description:''}</h3>
                 
                 </NavLink>
