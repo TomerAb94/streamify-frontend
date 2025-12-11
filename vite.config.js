37d6f6d210ba4ebdf9a0d73dc07476b7b996292b
Dorazar <dorazar01@gmail.com>
2025-12-11 11:43:01 +0200

    fix login

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	server: {
+		host: true,
+		port: 5173,
+	},
 	// After setting-up a backend, this can automoate the copying process of the built files:
 	// build: {
 	// 	outDir: '../backend/public',
