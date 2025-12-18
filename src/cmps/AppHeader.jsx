dcf24b920a169224af21cdc6b8fde8c8cdb4a039
Dorazar <dorazar01@gmail.com>
2025-12-18 17:41:00 +0200

    unregister service worker

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -6,7 +6,7 @@ import { useSelector } from 'react-redux'
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
 import { logout } from '../store/actions/user.actions'
 import { SvgIcon } from './SvgIcon'
-import { debounce,useDebounce } from '../services/util.service'
+import { debounce, useDebounce } from '../services/util.service'
 
 export function AppHeader() {
   const user = useSelector((storeState) => storeState.userModule.user)
@@ -33,11 +33,11 @@ export function AppHeader() {
   }, [searchBy])
 
   // window.filterBy = filterBy
-  
- function handleInput({ target }) {
-  const txt = target.value
-  debouncedSearch(txt)
-}
+
+  function handleInput({ target }) {
+    const txt = target.value
+    debouncedSearch(txt)
+  }
 
   function backToHome() {
     navigate('/')
@@ -71,35 +71,33 @@ export function AppHeader() {
     <div className="app-header">
       <div className="logo" title="Streamify" onClick={onToggleHomeBtn}>
         <NavLink to="/">
-<svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+          <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+            {/* <!-- Background circle with outline --> */}
+            <circle
+              cx="256"
+              cy="256"
+              r="240"
+              fill="#1DB954"
 
-  {/* <!-- Background circle with outline --> */}
-  <circle cx="256" cy="256" r="240" fill="#1DB954"
-  
-  // stroke="black" stroke-width="20"
-  />
+              // stroke="black" stroke-width="20"
+            />
 
-  {/* <!-- 8 uneven, messy bars --> */}
-  <rect x="60" y="260" width="25" height="110" rx="10" fill="black"/>
-  <rect x="90" y="210" width="25" height="160" rx="10" fill="black"/>
-  <rect x="120" y="285" width="25" height="85"  rx="10" fill="black"/>
-  <rect x="150" y="180" width="25" height="190" rx="10" fill="black"/>
-  <rect x="180" y="140" width="25" height="230" rx="10" fill="black"/>
-  <rect x="210" y="245" width="25" height="125" rx="10" fill="black"/>
-  <rect x="240" y="190" width="25" height="180" rx="10" fill="black"/>
-  <rect x="270" y="270" width="25" height="100" rx="10" fill="black"/>
-    <rect x="300" y="245" width="25" height="125" rx="10" fill="black"/>
-      <rect x="332" y="190" width="25" height="180" rx="10" fill="black"/>
-  <rect x="365" y="270" width="25" height="100" rx="10" fill="black"/>
-  <rect x="395" y="245" width="25" height="125" rx="10" fill="black"/>
-  <rect x="425" y="270" width="25" height="100" rx="10" fill="black"/>
-  
-
-</svg>
-
-
+            {/* <!-- 8 uneven, messy bars --> */}
+            <rect x="60" y="260" width="25" height="110" rx="10" fill="black" />
+            <rect x="90" y="210" width="25" height="160" rx="10" fill="black" />
+            <rect x="120" y="285" width="25" height="85" rx="10" fill="black" />
+            <rect x="150" y="180" width="25" height="190" rx="10" fill="black" />
+            <rect x="180" y="140" width="25" height="230" rx="10" fill="black" />
+            <rect x="210" y="245" width="25" height="125" rx="10" fill="black" />
+            <rect x="240" y="190" width="25" height="180" rx="10" fill="black" />
+            <rect x="270" y="270" width="25" height="100" rx="10" fill="black" />
+            <rect x="300" y="245" width="25" height="125" rx="10" fill="black" />
+            <rect x="332" y="190" width="25" height="180" rx="10" fill="black" />
+            <rect x="365" y="270" width="25" height="100" rx="10" fill="black" />
+            <rect x="395" y="245" width="25" height="125" rx="10" fill="black" />
+            <rect x="425" y="270" width="25" height="100" rx="10" fill="black" />
+          </svg>
         </NavLink>
-    
       </div>
 
       <div className="search-bar">
@@ -129,7 +127,7 @@ export function AppHeader() {
             id="upper-search"
             placeholder="What do you want to play?"
             onInput={(ev) => {
-            handleInput(ev)
+              handleInput(ev)
             }}
           />
 
@@ -149,8 +147,6 @@ export function AppHeader() {
         </div>
       </div>
 
-          
-
       <div className="user-profile">
         {!user && (
           <>
@@ -166,17 +162,21 @@ export function AppHeader() {
         {user && (
           <button className="user-info">
             {/* <Link to={`user/${user._id}`}> */}
-              {user.imgUrl && <img src={user.imgUrl} alt="user" onClick={onLogout}/>}
-              {/* {user.fullname} */}
+            {user.imgUrl && <img src={user.imgUrl} alt="user" onClick={onLogout} />}
+            {/* {user.fullname} */}
             {/* </Link> */}
             {/* <button onClick={onLogout}>logout</button> */}
           </button>
         )}
       </div>
- <div className="stations-type"><button className="active">All</button><button>Music</button><button>Podcasts</button></div>
-   <div className='back-btn' onClick={() => navigate(-1)}><SvgIcon iconName="back"/></div>
-   
+      <div className="stations-type">
+        <button className="active">All</button>
+        <button>Music</button>
+        <button>Podcasts</button>
+      </div>
+      <div className="back-btn" onClick={() => navigate(-1)}>
+        <SvgIcon iconName="back" />
+      </div>
     </div>
-    
   )
 }
